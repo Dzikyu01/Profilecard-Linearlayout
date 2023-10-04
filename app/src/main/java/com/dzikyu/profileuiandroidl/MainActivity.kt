430f516482aa14fa52736c7c0ac9cb904b5945b4
Dzikyu01 <dzikralisafadli@gmail.com>
2023-10-04 09:36:53 +0700

    Profile Card Linear

---
+++ app/src/main/java/com/dzikyu/profileuiandroidl/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dzikyu.profileuiandroidl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
